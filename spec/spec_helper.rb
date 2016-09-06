a0310164ed5bc3beb23ea9eaf20125e2b68712b8
hta-wtag <tanbir2025@gmail.com>
2016-09-06 11:37:54 +0600

    spec_helper updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'factory_girl_rails'
-require 'paperclip/matchers'
 ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include Paperclip::Shoulda::Matchers
