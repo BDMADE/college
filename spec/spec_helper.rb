590c33092b67ff2add5c6f218a6a4e1e2f13c8d4
hta-wtag <tanbir2025@gmail.com>
2016-09-06 11:25:30 +0600

    add rspec configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+require 'factory_girl_rails'
+require 'paperclip/matchers'
+ActiveRecord::Migration.maintain_test_schema!
+RSpec.configure do |config|
+  config.include Paperclip::Shoulda::Matchers
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
