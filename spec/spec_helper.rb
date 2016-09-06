659d3cc8666f22a5fba36c946e9d17ac0677d593
hta-wtag <tanbir2025@gmail.com>
2016-09-06 11:48:48 +0600

    spec_helper updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'factory_girl_rails'
 ActiveRecord::Migration.maintain_test_schema!
-RSpec.configure do |config|
-  config.include Paperclip::Shoulda::Matchers
+RSpec.configure do |config|  
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
