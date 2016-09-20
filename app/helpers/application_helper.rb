a9a0bd45d985938b416f8cc0de89700558658fc0
azizbabul <ahaque436@gmail.com>
2016-09-20 10:46:00 -0400

    code for rspec test added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def self.num
+    x = 5
+  end
 end
--- spec/models/admin/department_spec.rb
+++ spec/models/admin/department_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Admin::Department, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :short_form }
+  it { is_expected.to validate_uniqueness_of :name }
+  it { is_expected.to validate_uniqueness_of :short_form }
 end
+
