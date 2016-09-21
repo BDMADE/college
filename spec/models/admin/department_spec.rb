a1d53e256ce94644b597aa34012503639be8027e
azizbabul <ahaque436@gmail.com>
2016-09-21 15:38:09 -0400

    test code added

--- spec/models/admin/department_spec.rb
+++ spec/models/admin/department_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Admin::Department, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  it { is_expected.to validate_presence_of :name }
-  it { is_expected.to validate_presence_of :short_form }
-  it { is_expected.to validate_uniqueness_of :name }
-  it { is_expected.to validate_uniqueness_of :short_form }
-end
 
+  describe 'this test only passes' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :short_form }
+    it { is_expected.to validate_uniqueness_of :name }
+    it { is_expected.to validate_uniqueness_of :short_form }
+
+  end
+
+end
