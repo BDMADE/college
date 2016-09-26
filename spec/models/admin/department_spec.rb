8d62da0468d992d801ac86a7ce2d3e54508a2971
azizbabul <ahaque436@gmail.com>
2016-09-26 12:54:28 -0400

    removed code skip it from views/admin/departments/index html.rb

--- spec/models/admin/department_spec.rb
+++ spec/models/admin/department_spec.rb
@@ -1,22 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Admin::Department, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
   context 'this test only passes' do
 
-    it "should require a rname" do
-      Department.new(:name => "").should_not be_valid
-    end
-end
-  context 'this test only passes' do
-    it { is_expected.to validate_presence_of :name}
-end
-xcontext 'this test only passes' do
+    it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :short_form }
-end
-  context 'this test only passes' do
     it { is_expected.to validate_uniqueness_of :name }
     it { is_expected.to validate_uniqueness_of :short_form }
-
   end
     end
