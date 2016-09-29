a57620f7c3481c5d3aa57d49aa1df952684c398f
azizbabul <ahaque436@gmail.com>
2016-09-28 21:21:13 -0400

    duplicate migrtion file error in migration --add_attachment_image_to_admin

---
+++ app/models/admin/about.rb
@@ -0,0 +1,2 @@
+class Admin::About < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources :abouts
+  end
+  namespace :admin do
     resources :semesters,:departments
   end
   #namespace :admin do
