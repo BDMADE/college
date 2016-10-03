45bbf74606433186e50d57f36e635a7b5b06cab8
azizbabul <ahaque436@gmail.com>
2016-10-03 16:55:21 -0400

    uniqeness of slug added

--- app/models/admin/about.rb
+++ app/models/admin/about.rb
@@ -2,4 +2,5 @@ class Admin::About < ApplicationRecord
   has_attached_file :image, styles: { large:'600x600>',medium: '300x300>', thumb: '100x100>' }, default_url: '/assets/rails.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 validates_presence_of :slug
+  validates :slug, uniqueness: true
 end
