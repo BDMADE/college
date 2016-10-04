b7bbe0b26b6f87309ea6b3544b8bb1a10401b060
Tanbir Hasan <tanbir2025@gmail.com>
2016-10-04 11:50:25 +0600

    Update about_spec.rb

--- spec/models/admin/about_spec.rb
+++ spec/models/admin/about_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Admin::About, type: :model do
 
 
   it { is_expected.to validate_presence_of :slug }
+  it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_uniqueness_of :slug }
 end
