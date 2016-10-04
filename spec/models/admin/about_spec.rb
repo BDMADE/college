54307a86755d98415f309b43b62927e40e6af945
Tanbir Hasan <tanbir2025@gmail.com>
2016-10-04 11:54:56 +0600

    fix identation on about_spec.rb

--- spec/models/admin/about_spec.rb
+++ spec/models/admin/about_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Admin::About, type: :model do
-
-
   it { is_expected.to validate_presence_of :slug }
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_uniqueness_of :slug }
