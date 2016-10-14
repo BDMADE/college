86ceb173d0a8a176cd63f7dd69cd9adc52e7ed71
hmtanbir <tanbir2025@gmail.com>
2016-10-14 13:07:52 +0600

    DRY in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :notice_boards
+    resources :semesters, :departments, :abouts, :notice_boards
   end
-  #namespace :admin do
-   # resources :abouts
- # end
-  namespace :admin do
-    resources :semesters,:departments,:abouts
-  end
-  #namespace :admin do
-   # resources :departments
- # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
