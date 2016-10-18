c72b287a6e59cd4dc85d375f1e53bf62cf2db4d7
azizbabul <ahaque436@gmail.com>
2016-10-18 07:15:14 -0400

    uniqueness added

--- spec/models/admin/admission_eligibility_spec.rb
+++ spec/models/admin/admission_eligibility_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Admin::AdmissionEligibility, type: :model do
   context 'Admission eligibility model' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :description }
+    it { is_expected.to vallidate_uniqueness_of :name }
+
   end
 end
