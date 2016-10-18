846d088a0d15f290fad6d7fa3f098122c6d840d3
azizbabul <ahaque436@gmail.com>
2016-10-18 09:44:47 -0400

    test included

--- spec/views/admin/admission_eligibilities/index.html.erb_spec.rb
+++ spec/views/admin/admission_eligibilities/index.html.erb_spec.rb
@@ -6,17 +6,14 @@ RSpec.describe "admin/admission_eligibilities/index", type: :view do
       Admin::AdmissionEligibility.create!(
         :name => "Name",
         :description => "MyText"
-      ),
-      Admin::AdmissionEligibility.create!(
-        :name => "Name",
-        :description => "MyText"
       )
+
     ])
   end
 
   it "renders a list of admin/admission_eligibilities" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 1
+    assert_select "tr>td", :text => "MyText".to_s, :count => 1
   end
 end
