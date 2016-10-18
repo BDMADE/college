3b63d174dd5e4e38e9472a8dfeec322c6645b7bf
Tanbir Hasan <tanbir2025@gmail.com>
2016-10-18 10:10:18 +0600

    remove space from schema

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20161016173812) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "admin_semesters", force: :cascade do |t|
     t.string   "name"
     t.string   "short_form"
