334dd9fb7534087c41527261ce78e3a0dd153010
azizbabul <ahaque436@gmail.com>
2016-10-18 09:27:32 -0400

    changed made for uniqueness test of name and deleted extra line from model/spec files

--- spec/models/admin/admission_eligibility_spec.rb
+++ spec/models/admin/admission_eligibility_spec.rb
@@ -5,6 +5,5 @@ RSpec.describe Admin::AdmissionEligibility, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to vallidate_uniqueness_of :name }
-
   end
 end
