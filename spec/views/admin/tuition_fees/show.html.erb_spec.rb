f2d7623129e7a3993d9fffb7c1481c38d336e228
azizbabul <ahaque436@gmail.com>
2016-10-20 10:08:57 -0400

    done yet to test

---
+++ spec/views/admin/tuition_fees/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "admin/tuition_fees/show", type: :view do
+  before(:each) do
+    @admin_tuition_fee = assign(:admin_tuition_fee, Admin::TuitionFee.create!(
+      :name => "Name",
+      :payable => 2,
+      :group => nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(//)
+  end
+end
