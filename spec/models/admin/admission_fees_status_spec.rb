d9ce1c96a7857fa32210667dbf35739f21a52e19
azizbabul <ahaque436@gmail.com>
2016-10-21 17:16:26 -0400

    final commit

--- spec/models/admin/admission_fees_status_spec.rb
+++ spec/models/admin/admission_fees_status_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Admin::AdmissionFeesStatus, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :value }
+  it { is_expected.to validate_uniqueness_of :name }
+  it { is_expected.to validate_uniqueness_of :value }
+
 end
