6abd92f2d50f2015dbd827ef413df019d3d17ad7
Tanbir Hasan <tanbir2025@gmail.com>
2016-10-21 17:33:51 +0600

    :dog:

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
- # namespace :admin do
-    #resources :tuition_fees_groups
- # end
-  #namespace :admin do
-   # resources :abouts
- # end
-  namespace :admin do
-    resources :semesters,:departments,:abouts,:tuition_fees_groups,:notice_boards,:admission_eligibilities
+   namespace :admin do
+    resources :semesters,:departments,:abouts,
+              :notice_boards,:admission_eligibilities,:tuition_fees_groups
   end
-  #namespace :admin do
-   # resources :departments
- # end
-   # resources :semesters,:departments,:abouts,:admission_eligibilities
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
