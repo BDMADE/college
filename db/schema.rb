1936422d8b299c3215c03e7be2f84754210d85d4
azizbabul <ahaque436@gmail.com>
2016-10-21 08:05:02 -0400

    tution fee table removed

--- db/schema.rb
+++ db/schema.rb
@@ -71,19 +71,4 @@ ActiveRecord::Schema.define(version: 20161020194921) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "admin_tuition_fees", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "payable"
-    t.integer  "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_admin_tuition_fees_on_group_id"
-  end
-
-  create_table "admin_tuition_fees_groups", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
