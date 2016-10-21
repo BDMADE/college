9ed7cd50b411332c6af0c6f99832cb585986ca3c
azizbabul <ahaque436@gmail.com>
2016-10-21 08:39:24 -0400

    all fixed including test

--- app/models/admin/admission_fees_status.rb
+++ app/models/admin/admission_fees_status.rb
@@ -1,4 +1,5 @@
 class Admin::AdmissionFeesStatus < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :value
+  validates_uniqueness_of :name
 end
--- spec/views/admin/admission_fees_statuses/index.html.erb_spec.rb
+++ spec/views/admin/admission_fees_statuses/index.html.erb_spec.rb
@@ -6,17 +6,14 @@ RSpec.describe "admin/admission_fees_statuses/index", type: :view do
       Admin::AdmissionFeesStatus.create!(
         :name => "Name",
         :value => 2
-      ),
-      Admin::AdmissionFeesStatus.create!(
-        :name => "Name",
-        :value => 2
       )
+
     ])
   end
 
   it "renders a list of admin/admission_fees_statuses" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 1
+    assert_select "tr>td", :text => 2.to_s, :count => 1
   end
 end
