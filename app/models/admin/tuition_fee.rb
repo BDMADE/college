4af816b5283a2808783dfee63d774ff898cf5f64
azizbabul <ahaque436@gmail.com>
2016-10-24 11:19:16 -0400

    after DRY prac of routes and class name correction

--- app/models/admin/tuition_fee.rb
+++ app/models/admin/tuition_fee.rb
@@ -1,6 +1,7 @@
 class Admin::TuitionFee < ApplicationRecord
 
-  belongs_to :group, :class_name => Admin::AdmissionFeesStatus
+  belongs_to :group, :class_name => class Admin::TuitionFeesGroup
   validates_presence_of  :name
   validates_presence_of :payable
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :semesters, :departments, :abouts, :notice_boards,:admission_eligibilities,:tuition_fees_groups,:tuition_fees
+    resources :semesters, :departments, :abouts, :notice_boards,:admission_eligibilities,:tuition_fees_groups,:tuition_fees,:admission_fees_statuses
   #namespace :admin do
     #resources :admission_fees_statuses
   #end
-   namespace :admin do
-    resources :semesters,:departments,:abouts,
-              :notice_boards,:admission_eligibilities,:tuition_fees_groups,:admission_fees_statuses
-  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 end
