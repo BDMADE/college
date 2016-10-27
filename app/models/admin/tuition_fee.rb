5b065405bb526e0a902d05f0abdd33c4d7fa92b3
Tanbir Hasan <tanbir2025@gmail.com>
2016-10-27 13:25:47 +0600

    update tuition_fee.rb

--- app/models/admin/tuition_fee.rb
+++ app/models/admin/tuition_fee.rb
@@ -2,5 +2,6 @@ class Admin::TuitionFee < ApplicationRecord
 
   belongs_to :group, :class_name => Admin::TuitionFeesGroup
   validates_presence_of :name
+  validates_uniqueness_of :name
   validates_presence_of :payable
 end
