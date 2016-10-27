839c91cd0335bb7a719319510a7b6d6dbd8e4905
Tanbir Hasan <tanbir2025@gmail.com>
2016-10-27 13:29:22 +0600

    update index.html.erb_spec.rb

--- spec/views/admin/tuition_fees/index.html.erb_spec.rb
+++ spec/views/admin/tuition_fees/index.html.erb_spec.rb
@@ -7,19 +7,14 @@ RSpec.describe "admin/tuition_fees/index", type: :view do
         :name => "Name",
         :payable => 2,
         :group => FactoryGirl.create(:admin_tuition_fees_group)
-      ),
-      Admin::TuitionFee.create!(
-        :name => "Name",
-        :payable => 2,
-        :group => FactoryGirl.create(:admin_tuition_fees_group,name: "az" )
-      )
+      )      
     ])
   end
 
   it "renders a list of admin/tuition_fees" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 1
+    assert_select "tr>td", :text => 2.to_s, :count => 1
 
   end
 end
