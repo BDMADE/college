e9678a082ac766c2e2ebdd1ab303fd689a5c15f0
Tanbir Hasan <tanbir2025@gmail.com>
2016-10-27 13:17:45 +0600

    revert tuition_fees_group.rb

--- app/models/admin/tuition_fees_group.rb
+++ app/models/admin/tuition_fees_group.rb
@@ -1,6 +1,4 @@
 class Admin::TuitionFeesGroup < ApplicationRecord
   validates_presence_of :name
-  validates_uniqueness_of :name
-  validates_presence_of :payable
-  has_many :admin_tution_fees
+  validates_uniqueness_of :name  
 end
