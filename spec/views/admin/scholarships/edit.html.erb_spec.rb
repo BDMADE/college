9a90d5f6450baf46bae640f6c1655a5f9ae384aa
azizbabul <ahaque436@gmail.com>
2016-11-04 11:04:32 -0400

    see the reply on comment . also edit is fixed accept #assert_select input#admin_scholarship_group_id[name=?], admin_scholarship[group_id]

--- spec/views/admin/scholarships/edit.html.erb_spec.rb
+++ spec/views/admin/scholarships/edit.html.erb_spec.rb
@@ -13,20 +13,20 @@ RSpec.describe "admin/scholarships/edit", type: :view do
   end
 
 
- # it "renders the edit admin_scholarship form" do
-    #render
+  it "renders the edit admin_scholarship form" do
+    render
 
-   # assert_select "form[action=?][method=?]", admin_scholarship_path(@admin_scholarship), "post" do
+    assert_select "form[action=?][method=?]", admin_scholarship_path(@admin_scholarship), "post" do
 
-     # assert_select "input#admin_scholarship_name[name=?]", "admin_scholarship[name]"
+      assert_select "input#admin_scholarship_name[name=?]", "admin_scholarship[name]"
 
-      #assert_select "input#admin_scholarship_discount[name=?]", "admin_scholarship[discount]"
+      assert_select "input#admin_scholarship_discount[name=?]", "admin_scholarship[discount]"
 
-      #assert_select "input#admin_scholarship_condition[name=?]", "admin_scholarship[condition]"
+      assert_select "input#admin_scholarship_condition[name=?]", "admin_scholarship[condition]"
 
-     # assert_select "textarea#admin_scholarship_details[name=?]", "admin_scholarship[details]"
+     assert_select "textarea#admin_scholarship_details[name=?]", "admin_scholarship[details]"
 
       #assert_select "input#admin_scholarship_group_id[name=?]", "admin_scholarship[group_id]"
-    #end
-  #end
+    end
+  end
 end
