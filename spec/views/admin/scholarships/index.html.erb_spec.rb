1d76156dd3bd0fd863999b6b52a2522477f4dce7
azizbabul <ahaque436@gmail.com>
2016-11-05 16:46:03 -0400

    comments fixed

--- spec/views/admin/scholarships/index.html.erb_spec.rb
+++ spec/views/admin/scholarships/index.html.erb_spec.rb
@@ -20,6 +20,6 @@ RSpec.describe "admin/scholarships/index", type: :view do
     assert_select "tr>td", :text => 2.to_s, :count => 1
     assert_select "tr>td", :text => "Condition".to_s, :count => 1
     assert_select "tr>td", :text => "MyText".to_s, :count => 1
-    #assert_select "tr>td", :text => nil.to_s, :count => 1
+    assert_select "tr>td", :text => "az".to_s, :count => 1
   end
 end
