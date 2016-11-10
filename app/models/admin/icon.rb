c1fb534d957381aee2a01227d2d86fd866cc1a8e
Tanbir Hasan <tanbir2025@gmail.com>
2016-11-10 10:31:46 +0600

    extra line reduce in icon.rb

--- app/models/admin/icon.rb
+++ app/models/admin/icon.rb
@@ -1,7 +1,5 @@
 class Admin::Icon < ApplicationRecord
-
-  has_many :admin_events, :class_name=> 'Admin::Event',:inverse_of=>'admin_icons'
-
+  has_many :admin_events, class_name: 'Admin::Event',:inverse_of=>'admin_icons'
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_presence_of :icon
