c695d9bfe3f04bed1c2ac7664d3671972b486192
Tanbir Hasan <tanbir2025@gmail.com>
2016-11-12 06:33:05 +0600

    DRY at routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :websites
-  end
-  namespace :admin do
     resources :semesters, :departments, :abouts, :notice_boards,:admission_eligibilities,:tuition_fees_groups,:tuition_fees,
-              :admission_fees_statuses,:scholarship_groups,:scholarships,:courses,:news,:icons,:events
+              :admission_fees_statuses,:scholarship_groups,:scholarships,:courses,:news,:icons,:events,:websites
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
