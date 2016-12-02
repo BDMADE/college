3799b5660f9876adc6d5ccdf98a78c0d0d5278a7
azizbabul <ahaque436@gmail.com>
2016-12-02 11:24:05 -0500

    fixed as advised

--- db/migrate/20161130152451_create_admin_admission_process_image_details.rb
+++ db/migrate/20161130152451_create_admin_admission_process_image_details.rb
@@ -1,11 +1,12 @@
 class CreateAdminAdmissionProcessImageDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :admin_admission_process_image_details do |t|
-      #t.references :admission_process_image, foreign_key: true
-      t.text :details
       t.references :admission_process_image, foreign_key: true
+      t.text :details
+      t.index :name
+     # t.references :admission_process_image, foreign_key: true
       t.timestamps
     end
-    add_index :admission_process_image_details,[:name]
+   # add_index :admission_process_image_details,[:name]
   end
 end
