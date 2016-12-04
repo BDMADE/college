285f98d4edbb89d322ce3346b0003ceb0c4964e6
azizbabul <ahaque436@gmail.com>
2016-12-04 10:47:41 -0500

    index added

--- db/migrate/20161130152326_create_admin_admission_process_images.rb
+++ db/migrate/20161130152326_create_admin_admission_process_images.rb
@@ -1,7 +1,8 @@
 class CreateAdminAdmissionProcessImages < ActiveRecord::Migration[5.0]
   def change
     create_table :admin_admission_process_images do |t|
-      t.references :admission_process, foreign_key: true
+      t.references :admission_process, foreign_key: true,
+                   index: {name: :admission_process_image_}
 
       t.timestamps
     end
--- db/migrate/20161130152451_create_admin_admission_process_image_details.rb
+++ db/migrate/20161130152451_create_admin_admission_process_image_details.rb
@@ -1,12 +1,11 @@
 class CreateAdminAdmissionProcessImageDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :admin_admission_process_image_details do |t|
-      t.references :admission_process_image, foreign_key: true
+      t.references :admission_process_image, foreign_key: true,
+                   index: {name: :admission_process_image_details}
       t.text :details
       t.index :name
-     # t.references :admission_process_image, foreign_key: true
       t.timestamps
     end
-   # add_index :admission_process_image_details,[:name]
   end
 end
