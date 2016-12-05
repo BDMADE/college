cb20f21e7a903e0d5a38df6a9fdc7699f8d83c9e
azizbabul <ahaque436@gmail.com>
2016-12-05 12:03:46 -0500

    done with test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :admission_processes
-  end
-  namespace :admin do
     resources :semesters, :departments, :abouts, :notice_boards,:admission_eligibilities,:tuition_fees_groups,:tuition_fees,
               :admission_fees_statuses,:scholarship_groups,:scholarships,:courses,:news,:icons,:events,:websites,
-              :quick_links,:testimonials,:google_maps,:postal_types,:postals,:videos,:social_media
+              :quick_links,:testimonials,:google_maps,:postal_types,:postals,:videos,:social_media,:admission_processes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
