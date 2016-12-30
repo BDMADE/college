c5eacb5ac2489c0056850c9aa3cdc89014aac22c
azizbabul <ahaque436@gmail.com>
2016-12-30 12:39:43 -0500

    before push

--- app/models/admin/scholarship_group.rb
+++ app/models/admin/scholarship_group.rb
@@ -1,11 +1,9 @@
 class Admin::ScholarshipGroup < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
-<<<<<<< HEAD
-  has_many :admin_scholarships, :class_name => 'Admin::Scholarship',inverse_of: :admin_scholarship_group
-  #has_many :scholarships, :class_name => 'Admin::Scholarship',inverse_of: :group, foreign_key: 'group_id'
 
-=======
+  #has_many :admin_scholarships, :class_name => 'Admin::Scholarship',inverse_of: :admin_scholarship_group
+
   has_many :scholarships, :class_name => 'Admin::Scholarship',inverse_of: :group, foreign_key: 'group_id'
->>>>>>> origin/master
+
 end
