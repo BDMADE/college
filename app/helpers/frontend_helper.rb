7f5b9b247306042f7444acd4562e2700b4362cf3
Tanbir Hasan <tanbir2025@gmail.com>
2017-01-09 22:31:37 +0600

    update

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -1,5 +1,17 @@
 module FrontendHelper
-  def total_tuition_fees(tuition)
+def scholarship_style_change(scholarship)
+  array = []
+  if scholarship.discount > 49
+    array = ['OliveDrab', 'box box-border']
+  elsif scholarship.discount < 26 && scholarship.id.even?
+    array = ['FireBush', 'box box-theme']
+  else
+    array = ['Mojo', 'box box-dark']
+  end
+    array
+  end
+
+def total_tuition_fees(tuition)
   i=0
   multiple_of_status_values = 1
   number = tuition.admin_admission_fees_statuses.count
