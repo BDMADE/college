e26cce52dc1373911b60c58f49bbb11ee7654232
Tanbir Hasan <tanbir2025@gmail.com>
2017-01-09 23:14:55 +0600

    test is done

--- spec/helpers/frontend_helper_spec.rb
+++ spec/helpers/frontend_helper_spec.rb
@@ -44,4 +44,32 @@ RSpec.describe FrontendHelper, type: :helper do
       end
     end
   end
+  
+  describe '#total_tuition_fees(tuition)' do
+    let!(:tuition_group) { FactoryGirl.create :admin_tuition_fees_group, name: 'new fees' }
+    let!(:admission_fees_status_1) { FactoryGirl.create :admin_admission_fees_status, name: 'Admission_fees' ,value: 100 }
+    let!(:admission_fees_status_2) { FactoryGirl.create :admin_admission_fees_status, name: 'second', value: 2 }
+    let(:tuition_fees) { FactoryGirl.create :admin_tuition_fee, name: 'admission fees',group_id: tuition_group.id, admin_admission_fees_statuses: [admission_fees_status_1,admission_fees_status_2] }
+    let(:total_payable) { 200 }
+
+    it 'returns total_payable' do
+      expect(total_tuition_fees(tuition_fees)).to eq(total_payable)
+    end
+  end
+
+  describe '#tuition_fees_badge(tuition)' do
+    let!(:tuition_group) { FactoryGirl.create :admin_tuition_fees_group, name: 'new fees' }
+    let!(:admission_fees_status_1) { FactoryGirl.create :admin_admission_fees_status, name: 'Admission_fees' ,value: 100 }
+    let!(:admission_fees_status_2) { FactoryGirl.create :admin_admission_fees_status, name: 'second', value: 2 }
+    let(:tuition_fees_1) { FactoryGirl.create :admin_tuition_fee, id:1, name: 'admission fees 1',group_id: tuition_group.id, admin_admission_fees_statuses: [admission_fees_status_1,admission_fees_status_2] }
+    let(:tuition_fees_2) { FactoryGirl.create :admin_tuition_fee, id:2, name: 'admission fees 2',group_id: tuition_group.id, admin_admission_fees_statuses: [admission_fees_status_1,admission_fees_status_2] }
+
+    it 'returns success badge for odd number id' do
+      expect(tuition_fees_badge(tuition_fees_1)).to eq('success')
+    end
+
+    it 'returns warning badge for odd number id' do
+      expect(tuition_fees_badge(tuition_fees_2)).to eq('warning')
+    end
+  end
 end
